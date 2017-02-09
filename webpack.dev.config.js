ec05c0067d6919ae375999eb579b37cee20ce6a3
dan-j <jonesd9@me.com>
2017-02-09 22:15:24 +0000

    fixed ESLint complaining about indentation

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,24 +3,25 @@ const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const baseConfig = require('./webpack.base.config');
 
-module.exports = () => webpackMerge({
-      entry: {
-        bundle: [
-          'react-hot-loader/patch',
-          'webpack/hot/only-dev-server',
-        ],
-      },
-
-      plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
+module.exports = () => webpackMerge(
+  {
+    entry: {
+      bundle: [
+        'react-hot-loader/patch',
+        'webpack/hot/only-dev-server',
       ],
-
-      devServer: {
-        historyApiFallback: true,
-        contentBase: path.resolve(__dirname, 'dist/'),
-        hot: true,
-      },
     },
-    baseConfig()
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ],
+
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.resolve(__dirname, 'dist/'),
+      hot: true,
+    },
+  },
+  baseConfig(),
 );
