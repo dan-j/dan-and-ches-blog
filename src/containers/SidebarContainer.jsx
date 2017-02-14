40342a2fb5a7d7e96bbe70381d3cea8f88582ccb
dan-j <jonesd9@me.com>
2017-02-14 01:25:56 +0000

    Think we've got a winner for fixed sidebar and centred content

--- src/containers/SidebarContainer.jsx
+++ src/containers/SidebarContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SidebarContainer = () => (
   <div>
-    <h3>Sidebar that's quite a bit longer than 268px!</h3>
+    <h3>Sidebar that&apos;s quite a bit longer than 268px!</h3>
   </div>
 );
 
